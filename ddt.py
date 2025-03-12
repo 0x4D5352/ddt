6398f6356595089995b7107ed6bd5a6a225c5426
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-03-12 14:52:20 -0500

    updates for feat enhancement

--- ddt.py
+++ ddt.py
@@ -1,6 +1,10 @@
 import argparse
 import tiktoken
 from pathlib import Path
+from math import ceil
+# import json
+# import csv
+# import yaml
 
 GPT_4O = "gpt-4o"
 GPT_4O_MINI = "gpt-4o-mini"
@@ -36,8 +40,7 @@ def main() -> None:
         print("ERROR: Path Provided Is Not A Directory")
         exit(1)
 
-    print("Parsing directory...")
-    print_separator()
+    print_with_separator("Parsing directory...", after=False)
     files = list(root.glob("**/*.*"))
     file_categories: dict[str, list[tuple[str, int]]] = {}
     category_totals: dict[str, int] = {}
@@ -73,14 +76,12 @@ def main() -> None:
 
     print("\nParsing complete!")
     for filetype, count in category_totals.items():
-        print_separator()
-        print(f"{filetype} tokens:")
+        print_with_separator(f"{filetype} tokens:")
         for file in file_categories[filetype]:
             print(f"{file[0]}: {file[1]:,} tokens")
         print(f"{filetype} total: {count:,} tokens")
 
-    print_separator()
-    print(f"grand total: {running_total:,}")
+    print_with_separator(f"grand total: {running_total:,}")
     print(f"remaining tokens given 128K context window: {128_000 - running_total:,}")
 
 
@@ -105,37 +106,73 @@ def setup_argparse() -> argparse.ArgumentParser:
         action="store_true",
         help="ignore directories and files with 'old' in the name.",
     )
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "-e",
-        "--exclude",
-        action="append",
-        help="specify file formats to ignore from counting. this flag may be set multiple times for multiple entries. cannot be set if including files",
-    )
-    group.add_argument(
-        "-i",
-        "--include",
-        action="append",
-        help="specify file formats to include when counting. this flag may be set multiple times for multiple entries. cannot bet set if excluding files",
-    )
     parser.add_argument(
-        "-m--model",
+        "-m",
+        "--model",
         action="store",
         help="specify a model to use for token approximation. default is 'gpt-4o'",
         choices=MODEL_CHOICES,
         default=GPT_4O,
     )
+    parser.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        help="save the results of the scan to a file. does not include stdout messages.",
+        # choices=["json", "csv", "yaml", "html"],
+    )
+    file_types_group = parser.add_mutually_exclusive_group()
+    file_types_group.add_argument(
+        "-e",
+        "--exclude",
+        action="append",
+        help="specify file formats to ignore from counting. this flag may be set multiple times for multiple entries. cannot be set if including files",
+    )
+    file_types_group.add_argument(
+        "-i",
+        "--include",
+        action="append",
+        help="specify file formats to include when counting. this flag may be set multiple times for multiple entries. cannot bet set if excluding files",
+    )
     return parser
 
 
-def print_separator() -> None:
+"""
+Logging/Stdout Helpers
+"""
+
+
+def print_with_separator(string: str, after: bool = True) -> None:
+    """
+    A helper method to add line separators between sections of the stdout log.
+
+    Args:
+        string(string): the text to print.
+        after(bool): If the text should print before or after the line separator. Defaults to after (True).
+    """
+    if not after:
+        print(string)
     print("-------------------------------------------------")
+    if after:
+        print(string)
 
 
 def print_if_verbose(string: str, is_verbose: bool) -> None:
+    """
+    A helper method that responds to the verbosity flag. is_verbose should always be set to the result of the is_verbose value in main()
+
+    Args:
+        string(string): the text to print.
+        is_verbose(bool): If the text should print.
+    """
     if not is_verbose:
         return
     print(string)
+
+
+"""
+Tokenizing methods
+"""
 
 
 def num_tokens_from_string(string: str, model_name: str) -> int:
@@ -143,6 +180,39 @@ def num_tokens_from_string(string: str, model_name: str) -> int:
     encoding = tiktoken.encoding_for_model(model_name)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
+
+# Source for image token code: https://medium.com/@teekaifeng/gpt4o-visual-tokenizer-an-illustration-c69695dd4a39
+"""
+Adapted from https://community.openai.com/t/how-do-i-calculate-image-tokens-in-gpt4-vision/492318/2 
+"""
+
+
+def calculate_image_tokens(width: int, height: int):
+    # Step 1: scale to fit within a 2048 x 2048 square (maintain aspect ratio)
+    if width > 2048 or height > 2048:
+        aspect_ratio = width / height
+        if aspect_ratio > 1:
+            width, height = 2048, int(2048 / aspect_ratio)
+        else:
+            width, height = int(2048 * aspect_ratio), 2048
+
+    # Step 2: scale such that the shortest side of the image is 768px long
+    if width >= height and height > 768:
+        width, height = int((768 / height) * width), 768
+    elif height > width and width > 768:
+        width, height = 768, int((768 / width) * height)
+
+    # Step 3: compute number of 512x512 tiles that can fit into the image
+    tiles_width = ceil(width / 512)
+    tiles_height = ceil(height / 512)
+
+    # See https://platform.openai.com/docs/guides/vision/calculating-costs
+    #   - 85 is the "base token" that will always be added
+    #   - 1 tiles = 170 tokens
+    total_tokens = 85 + 170 * (tiles_width * tiles_height)
+
+    return total_tokens
 
 
 if __name__ == "__main__":
