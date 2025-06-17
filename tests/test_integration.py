2f56f0ec4208cb052d17c87ae407a66a475546be
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-06-17 16:21:07 -0500

    dev: push more test scripts to fill out

---
+++ tests/test_integration.py
@@ -0,0 +1,8 @@
+"""
+    app.py
+"""
+
+# main function - basically integration/e2e testing???
+
+
+
---
+++ tests/test_parser.py
@@ -0,0 +1,44 @@
+from ddt import parser
+"""
+    parser.py
+"""
+# CLIParser Init
+
+def test_parser_initialization():
+    cli = parser.CLIParser()
+    pass
+
+
+# CLIParser Args
+
+# CLI positional - directory
+
+# CLI optional - config file
+
+# CLI optional - verbosity
+
+# CLI optional - gitignore
+
+# CLI optional - dotfiles
+
+# cli optional - symlinks
+
+# cli optional - images
+
+# cli optional - resolve relative
+
+# cli optional - model
+
+# cli optional - output
+
+# cli optional - output type - json
+
+# cli optional - output type - html
+
+# cli optional - filter group - inclusions
+
+# cli optinoal - filter group - exclusions
+
+# CLIParser Generate Config
+
+
