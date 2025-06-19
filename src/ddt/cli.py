1793b108fba261badde341ca918979d8c4421ad0
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-06-19 17:43:22 -0500

    deprecation: remove config.json support

--- src/ddt/cli.py
+++ src/ddt/cli.py
@@ -1,9 +1,7 @@
 from pathlib import Path
-from sys import exit as sysexit
-import json
 import argparse
 import sys
-from . import config, tokenizer
+from . import tokenizer
 
 def setup_argparse() -> argparse.ArgumentParser:
     """
@@ -15,59 +13,52 @@ def setup_argparse() -> argparse.ArgumentParser:
         epilog="Made with <3 by 0x4D5352",
     )
 
-    parser.add_argument(
+    _ = parser.add_argument(
         "directory",
         help="the relative or absolute path to the directory you wish to scan",
         type=Path,
     )
 
-    parser.add_argument(
-        "-c",
-        "--config",
-        action="store",
-        help="Load one or more configurations from a file. Unset configs will use defaults.",
-        type=argparse.FileType(mode="r", encoding="UTF-8"),
-    )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
         help="set to increase logging to console",
     )
 
-    parser.add_argument(
+    _ = parser.add_argument(
         "-g",
         "--include-gitignore",
         action="store_true",
         help="include files and directories found in the .gitignore file",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-d",
         "--include-dotfiles",
         action="store_true",
         help="include files and directories beginning with a dot (.)",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-s",
         "--include-symlinks",
         action="store_true",
         help="include files and directories symlinked from outside the target directory",
     )
-    parser.add_argument(
+    _ = parser.add_argument(
         "-i",
         "--include-images",
         action="store_true",
         help="include image files found within the directory",
     )
 
-    parser.add_argument(
+    _ = parser.add_argument(
         "-r",
         "--resolve-paths",
         action="store_true",
         help="resolve relative file paths to their absolute location",
     )
 
-    parser.add_argument(
+    _ = parser.add_argument(
         "-m",
         "--model",
         action="store",
@@ -77,7 +68,7 @@ def setup_argparse() -> argparse.ArgumentParser:
         type=tokenizer.Model,
     )
 
-    parser.add_argument(
+    _ = parser.add_argument(
         "-o",
         "--output",
         action="store",
@@ -87,84 +78,28 @@ def setup_argparse() -> argparse.ArgumentParser:
     )
 
     output_type_group = parser.add_mutually_exclusive_group()
-    output_type_group.add_argument(
+    _ = output_type_group.add_argument(
         "--json",
         action="store_true",
         help="save the results of the scan to a json file",
     )
-    output_type_group.add_argument(
+    _ = output_type_group.add_argument(
         "--html",
         action="store_true",
         help="save the results of the scan to a HTML file",
     )
 
     input_filter_group = parser.add_mutually_exclusive_group()
-    input_filter_group.add_argument(
+    _ = input_filter_group.add_argument(
         "--exclude",
         action="append",
         help="specify file formats to ignore from counting. this flag may be set multiple times for multiple entries. cannot be set if including files",
         type=str,
     )
-    input_filter_group.add_argument(
+    _ = input_filter_group.add_argument(
         "--include",
         action="append",
         help="specify file formats to include when counting. this flag may be set multiple times for multiple entries. cannot be set if excluding files",
         type=str,
     )
     return parser
-
-def generate_config(args: argparse.Namespace) -> config.Config:
-    """
-    Basically the main function of this class - it converts the CLI config into a system config:
-
-    - Fills out config with existing config file.
-    - Specifies output format.
-    - applies CLI args to config
-    - Resolves the root path and validates that it is a directory
-    - Generates and returns a config based on the CLI and JSON config
-    """
-    if args.config:
-        with args.config.open("r") as json_conf:
-            conf = json.load(json_conf)
-    else:
-        conf = dict()
-
-    if args.json:
-        output_format = "json"
-    elif args.html:
-        output_format = "html"
-    else:
-        output_format = "txt"
-
-    for arg, value in vars(args).items():
-        if arg == "config" or arg in conf.keys():
-            continue
-        conf[arg] = value
-
-    if conf["exclude"] and conf["include"]:
-        print(
-            "error: both inclusions and exclusions found. please remove one group"
-        )
-        sysexit(1)
-
-    root: Path = conf["directory"].resolve()
-    # TODO: replace with single file tokenization if a file instead of a directory
-    if not root.is_dir():
-        print("ERROR: Path Provided Is Not A Directory")
-        sysexit(1)
-
-    cfg = config.Config(
-        root=root,
-        is_verbose=conf["verbose"],
-        include_gitignore=conf["include_gitignore"],
-        include_dotfiles=conf["include_dotfiles"],
-        include_symlinks=conf["include_symlinks"],
-        include_images=conf["include_images"],
-        resolve_paths=conf["resolve_paths"],
-        model=conf["model"],
-        output=conf["output"],
-        output_format=output_format,
-        exclude=conf["exclude"],
-        include=conf["include"],
-    )
-    return cfg
