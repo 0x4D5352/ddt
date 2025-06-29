5c9c183a8834bf3c813ca0755e620d447894bdac
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-06-29 13:44:22 -0500

    almost to 100% code coverage, yay

--- tests/test_config.py
+++ tests/test_config.py
@@ -47,34 +47,69 @@ def test_config_happy_path():
 
 
 def test_generate_config():
-    args = {
-        "root": Path("."),
-        "verbose": True,
-        "include_gitignore": True,
-        "include_dotfiles": True,
-        "include_symlinks": True,
-        "include_images": True,
-        "resolve_paths": True,
-        "model": tokenizer.Model("gpt-4o"),
-        "output": sys.stdout,
-        "output_format": "txt",
-        "json": False,  # to fix test, goes away when output config changes
-        "html": False,  # to fix test, goes away when output config changes
-        "exclude": ["foo"],
-        "include": ["bar"],
-    }
+    args = [
+        {
+            "root": Path("."),
+            "verbose": True,
+            "include_gitignore": True,
+            "include_dotfiles": True,
+            "include_symlinks": True,
+            "include_images": True,
+            "resolve_paths": True,
+            "model": tokenizer.Model("gpt-4o"),
+            "output": sys.stdout,
+            "output_type": "txt",
+            "json": False,  # to fix test, goes away when output config changes
+            "html": False,  # to fix test, goes away when output config changes
+            "exclude": ["foo"],
+            "include": ["bar"],
+        },
+        {
+            "root": Path("."),
+            "verbose": True,
+            "include_gitignore": True,
+            "include_dotfiles": True,
+            "include_symlinks": True,
+            "include_images": True,
+            "resolve_paths": True,
+            "model": tokenizer.Model("gpt-4o"),
+            "output": sys.stdout,
+            "output_type": "json",
+            "json": True,  # to fix test, goes away when output config changes
+            "html": False,  # to fix test, goes away when output config changes
+            "exclude": ["foo"],
+            "include": ["bar"],
+        },
+        {
+            "root": Path("."),
+            "verbose": True,
+            "include_gitignore": True,
+            "include_dotfiles": True,
+            "include_symlinks": True,
+            "include_images": True,
+            "resolve_paths": True,
+            "model": tokenizer.Model("gpt-4o"),
+            "output": sys.stdout,
+            "output_type": "html",
+            "json": False,  # to fix test, goes away when output config changes
+            "html": True,  # to fix test, goes away when output config changes
+            "exclude": ["foo"],
+            "include": ["bar"],
+        },
+    ]
 
-    cfg = config.generate_config(args)
+    for argset in args:
+        cfg = config.generate_config(argset)
 
-    assert cfg.root == Path(".").resolve()
-    assert cfg.is_verbose
-    assert cfg.include_gitignore
-    assert cfg.include_dotfiles
-    assert cfg.include_symlinks
-    assert cfg.include_images
-    assert cfg.resolve_paths
-    assert cfg.model == tokenizer.Model("gpt-4o")
-    assert cfg.output == sys.stdout
-    assert cfg.output_format == "txt"
-    assert cfg.exclude == ["foo"]
-    assert cfg.include == ["bar"]
+        assert cfg.root == Path(".").resolve()
+        assert cfg.is_verbose
+        assert cfg.include_gitignore
+        assert cfg.include_dotfiles
+        assert cfg.include_symlinks
+        assert cfg.include_images
+        assert cfg.resolve_paths
+        assert cfg.model == tokenizer.Model("gpt-4o")
+        assert cfg.output == sys.stdout
+        assert cfg.output_format == argset["output_type"]
+        assert cfg.exclude == ["foo"]
+        assert cfg.include == ["bar"]
