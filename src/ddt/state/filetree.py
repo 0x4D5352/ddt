f882d76dd6df18807bf17c1c302eb37c28d23c68
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-07-19 09:19:45 -0500

    filetree is a state module

--- src/ddt/state/filetree.py
+++ src/ddt/state/filetree.py
@@ -3,4 +3,17 @@ Three data stuctures:
 All Files - a set of files (Path)
 Excluded files - a list/set of files (Path)
 Included files - a list/set of files (Path)
+
+Logic: 
+take a FileTree.All Files and separate it out to Excluded Files and Included Files
+
+Input:
+
+- Set of files
+
+Logic:
+
+1. filter excluded files to get initial included/excluded list
+2. if custom inclusions exist, filter included files to get added exclusions and new included list.
+3. Pass excluded to Result.NotCounted
 """
