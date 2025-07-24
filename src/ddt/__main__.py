b2975c581a5aa4da86864fc019bed20a138f0807
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-07-24 16:58:39 -0500

    alright that didn't work

--- src/ddt/__main__.py
+++ src/ddt/__main__.py
@@ -1,7 +1,5 @@
 import logging
-import sys
-from pathlib import Path
-from . import cli, config, models, tokenizer
+from . import cli, config, models
 
 """
 Main function
@@ -9,24 +7,24 @@ Main function
 
 
 def main() -> None:
-    if len(sys.argv) == 0:
-        standard_input = cli.read_stdin()
-        print(
-            tokenizer.calculate_text_tokens(standard_input, tokenizer.GPT_4O),
-            file=sys.stdout,
-        )
-        sys.exit(0)
+    # if len(sys.argv) == 0:
+    #     standard_input = cli.read_stdin()
+    #     print(
+    #         tokenizer.calculate_text_tokens(standard_input, tokenizer.GPT_4O),
+    #         file=sys.stdout,
+    #     )
+    #     sys.exit(0)
     p = cli.setup_argparse()
     args = p.parse_args()
-    target: Path = args.root
-    if not target.exists():
-        print(f"File '{target.name}' not found.", file=sys.stderr)
-        sys.exit(1)
-    if target.is_file():
-        file = cli.read_file(target)
-        model = args.model
-        print(tokenizer.calculate_text_tokens(file, model))
-
+    # target: Path = args.root
+    # if not target.exists():
+    #     print(f"File '{target.name}' not found.", file=sys.stderr)
+    #     sys.exit(1)
+    # if target.is_file():
+    #     file = cli.read_file(target)
+    #     model = args.model
+    #     print(tokenizer.calculate_text_tokens(file, model))
+    #
     cfg = config.generate_config(vars(args))
 
     token_counter = models.TokenCounter(cfg)
